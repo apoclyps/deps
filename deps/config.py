1818c90a44903d282b8e174c6467bd0623c76b7e
Kyle Harrison <kyle90adam@hotmail.com>
2022-12-09 18:06:03 +0000

    adds retry logic to external requests to pypi (#120)

--- deps/config.py
+++ deps/config.py
@@ -11,5 +11,11 @@ GITHUB_TOKEN = config("GITHUB_TOKEN", cast=str)
 GITHUB_URL = config("GITHUB_URL", cast=str, default="https://api.github.com")
 GITHUB_USER = config("GITHUB_USER", cast=str)
 
+# External Requests Config
+EXTERNAL_SERVICE_MAXIMUM_REQUEST_ATTEMPTS = config("EXTERNAL_SERVICE_MAXIMUM_REQUEST_ATTEMPTS", cast=int, default=3)
+EXTERNAL_SERVICE_WAIT_IN_MS_BETWEEN_REQUESTS = config(
+    "EXTERNAL_SERVICE_WAIT_IN_MS_BETWEEN_REQUESTS", cast=int, default=250
+)
+
 # Deps Config
 DEPS_EXPORT_TO_SVG = config("DEPS_EXPORT_TO_SVG", cast=bool, default=False)
