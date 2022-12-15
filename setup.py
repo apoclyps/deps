d73eee5830a5d160a8d618599799f77523a4c47b
Kyle Harrison <kyle90adam@hotmail.com>
2022-12-15 12:21:11 +0000

    Bump version: 0.2.0 → 0.2.1 (#128)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md") as fh:
 setup(
     name="deps",
     packages=find_namespace_packages(include=["*"]),
-    version="0.2.0",
+    version="0.2.1",
     license="MIT",
     description=("A terminal UI dashboard to view python dependencies across Github repositories."),
     author="Kyle Harrison",
